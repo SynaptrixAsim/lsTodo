55fa982a329203cdc799e12cddd93a09dbb9bd0b
Asim <asimsaifioffical12@gmail.com>
2025-10-22 13:12:07 +0530

    update name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="w-full flex justify-between p-2 mb-4 bg-purple-800 text-white">
       <div className="mx-4">
-        <span className="font-bold text-xl">iTodo</span>
+        <span className="font-bold text-xl">lsTodo</span>
       </div>
       <ul className="flex gap-5 mx-5">
         <li className="hover:cursor-pointer hover:font-bold">Home</li>
